0fdfecf1df37c01552b19760df23338e1c25db39
Anis KCHAOU <kchaouanis25@gmail.com>
2021-12-24 22:18:33 +0100

    Update SettingsRepository.java

--- src/main/java/com/dev/delta/repositories/SettingsRepository.java
+++ src/main/java/com/dev/delta/repositories/SettingsRepository.java
@@ -12,6 +12,6 @@ import com.dev.delta.entities.Settings;
 public interface SettingsRepository extends JpaRepository<Settings, Long> {
 	@Modifying
 	@Transactional
-	@Query(nativeQuery = true, value = "update settings s set s.lang=:lang  where s.id=1")
+	@Query(nativeQuery = true, value = "update settings  set lang=:lang  where id=1")
 	void updateSettingsByLang(@Param("lang") String lang);
 }
