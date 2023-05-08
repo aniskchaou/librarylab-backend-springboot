8839776ac308d05d8ab1f86b6d612d754de462e4
Anis KCHAOU <16862158+aniskchaou@users.noreply.github.com>
2023-05-08 10:49:42 +0200

    Update SpringbootlibraryLabCodecanyonApplication.java

--- src/main/java/com/dev/delta/SpringbootlibraryLabCodecanyonApplication.java
+++ src/main/java/com/dev/delta/SpringbootlibraryLabCodecanyonApplication.java
@@ -97,7 +97,7 @@ public class SpringbootlibraryLabCodecanyonApplication implements CommandLineRun
 	@Override
 	public void run(String... args) throws Exception {
 
-		/*bookStatusDTO.populate();
+		bookStatusDTO.populate();
 		memberDTO.populate();
 		circulationStatusDTO.populate();
 		publisherDTO.populate();
@@ -113,7 +113,7 @@ public class SpringbootlibraryLabCodecanyonApplication implements CommandLineRun
 		searchDTO.populate();
 		paymentDTO.populate();
 		incomeDTO.populate();
-		expenseDTO.populate();*/
+		expenseDTO.populate();
 
 	}
 
