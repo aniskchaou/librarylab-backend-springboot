eca3c7dcbfa9d73c6027f5faea657ecad633e1c2
Anis KCHAOU <16862158+aniskchaou@users.noreply.github.com>
2025-02-01 18:05:56 +0100

    Update WebConfig.java

--- src/main/java/com/dev/delta/security/WebConfig.java
+++ src/main/java/com/dev/delta/security/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import java.util.Collections;
 import java.util.List;
@@ -26,5 +27,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new MappingJackson2HttpMessageConverter());
+    }
+
+        @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Allow all origins for all endpoints
+        registry.addMapping("/**")
+                .allowedOrigins("https://librarylab-frontend-72f7a60f7ba2.herokuapp.com") // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // If you need to allow cookies
     }
 }
